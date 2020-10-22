caf7c2710485c6c8f99c5f19c72c4e3819ec5156
Kleberson <klebersonlr@gmail.com>
2020-10-22 18:02:26 -0300

    Criado atributo filho

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,21 @@
 class Pessoa:
-    print('Eu sou uma pessoa')
+    def __init__(self, *filhos, nome=None, idade=35): #dunder init
+        #atributos de instancia e objetos
+        #self.nomedoatributo = parametro ou variavel
+        self.idade = idade
+        self.nome = nome
+        self.filhos = list(filhos)
+
+    def cumprimentar(self):
+        return f'Olá{id(self)}'
+
+if __name__ == '__main__':
+    renzo = Pessoa(nome='Renzo')
+    luciano = Pessoa(renzo, nome='Luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
