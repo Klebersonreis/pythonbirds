7bf9fb9455f38bfa6304b228511df19815dd0a0c
Kleberson <klebersonlr@gmail.com>
2020-10-29 17:55:34 -0300

    Testes realizados com sucesso.

--- oo/carro.py
+++ oo/carro.py
@@ -130,8 +130,27 @@ class Motor:
         self.velocidade -= 2
         self.velocidade=max(0, self.velocidade)
 
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
 
+    def calcular_velocidade(self):
+        return self.motor.velocidade
 
+    def acelerar(self):
+        self.motor.acelerar()
 
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        return self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        return self.direcao.girar_a_esquerda()
 
     pass
