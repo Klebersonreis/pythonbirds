7932025db516d88c8774bdd654817db880affdb1
Kleberson <klebersonlr@gmail.com>
2020-11-04 08:58:39 -0300

    Testes ainda com falhas

--- fase.py
+++ fase.py
@@ -43,7 +43,7 @@ class Fase():
 
         :param obstaculos:
         """
-        pass
+        self._obstaculos.extend(obstaculos)
 
     def adicionar_porco(self, *porcos):
         """
@@ -51,7 +51,7 @@ class Fase():
 
         :param porcos:
         """
-        pass
+        self._obstaculos.extend(porcos)
 
     def adicionar_passaro(self, *passaros):
         """
@@ -59,8 +59,9 @@ class Fase():
 
         :param passaros:
         """
-        pass
+        self._obstaculos.extend(passaros)
 
+    @property
     def status(self):
         """
         Método que indica com mensagem o status do jogo
@@ -73,7 +74,12 @@ class Fase():
 
         :return:
         """
-        return EM_ANDAMENTO
+        if not self._possui_porco_ativo():
+            return DERROTA
+        elif self._possui_passaros_ativos():
+            return EM_ANDAMENTO
+        else:
+            return VITORIA
 
     def lancar(self, angulo, tempo):
         """
@@ -86,8 +92,10 @@ class Fase():
         :param angulo: ângulo de lançamento
         :param tempo: Tempo de lançamento
         """
-        pass
-
+        for passaro in self._passaros:
+            if not passaro.foi_lancado():
+                passaro.lancar(angulo, tempo)
+                break
 
     def calcular_pontos(self, tempo):
         """
@@ -105,3 +113,14 @@ class Fase():
     def _transformar_em_ponto(self, ator):
         return Ponto(ator.x, ator.y, ator.caracter())
 
+    def _possui_porco_ativo(self):
+        for porco in self._porcos:
+            if porco.status == ATIVO:
+                return True
+            return False
+
+    def _possui_passaros_ativos(self):
+        for passaro in self._passaros:
+            if passaro.status == ATIVO:
+                return True
+        return False
